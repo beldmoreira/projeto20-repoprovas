e8f49f55f091dc95f53ec87de717a4edee55f1a5
Isabela <isabela_cmoreira@hotmail.com>
2022-07-25 16:15:28 -0300

    feat: created authentication factory

---
+++ tests/factories/userFactory.ts
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import { faker } from '@faker-js/faker';
+import {prisma} from "../../src/config/database.js";
+
+
+function createLogin(email = "beld@gmail.com", passwordLength = 10) {
+  return {
+    email,
+    password: faker.internet.password(passwordLength)
+  }
+}
+
+interface Login {email: string, password: string};
+
+async function createUser(login: Login) {
+  const user = await prisma.user.create({
+    data: {
+      email: login.email,
+      password: bcrypt.hashSync(login.password, 12)
+    }
+  });
+
+  return {...user, plainPassword: login.password};
+}
+
+const userFactory = {
+  createLogin,
+  createUser
+}
+
+export default userFactory;
