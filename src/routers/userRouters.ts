428685762963abf63229688de4862a43e5ce0a8f
Isabela <isabela_cmoreira@hotmail.com>
2022-07-25 09:11:13 -0300

    feat: created routes related to the user

---
+++ src/routers/userRouters.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import userController from "../controllers/userController.js";
+import joiValidation from "../middlewares/joiValidation.js";
+import { userSchema } from "../schemas/userSchemas.js";
+
+const userRouter = Router();
+
+userRouter.post("/signin", joiValidation(userSchema),userController.signIn)
+userRouter.post("/signup", joiValidation(userSchema),userController.signUp)
+
+export default userRouter;
