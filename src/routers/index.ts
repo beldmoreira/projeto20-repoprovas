d8716077c100e7c29bc47a5de65a2db27cd46f02
Isabela <isabela_cmoreira@hotmail.com>
2022-07-25 13:23:27 -0300

    feat: added the tests router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRouters.js";
 import categoryRouter from "./categoriesRouters.js";
+import testsRouter from "../routers/testsRouters.js"
 
 const router = Router();
 
 router.use(userRouter);
 router.use(categoryRouter);
+router.use(testsRouter);
 
 export default router;
