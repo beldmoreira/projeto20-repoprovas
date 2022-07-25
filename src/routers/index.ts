a56d0bb5fc5f9e23c6edc05a9c756a6a68835797
Isabela <isabela_cmoreira@hotmail.com>
2022-07-25 12:46:59 -0300

    feat: added some changes to the routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouters.js";
+import categoryRouter from "./categoriesRouters.js";
 
 const router = Router();
 
 router.use(userRouter);
+router.use(categoryRouter);
 
 export default router;
--- src/routers/userRouters.ts
+++ src/routers/userRouters.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
 import joiValidation from "../middlewares/joiValidation.js";
+import {ensureAuthenticatedMiddleware} from "../middlewares/authMiddleware.js"
 import { userSchema } from "../schemas/userSchemas.js";
 
 const userRouter = Router();
